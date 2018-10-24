e7c273958965834a79b85fc3e04df1014d3817c4
Kin_dada <mungekar.kiran30@gmail.com>
2018-10-24 16:54:13 +0530

    passportJs installed

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express= require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const PORT = process.env.PORT || 5000;
+
+passport.use(new GoogleStrategy());
 
 app.get('/',(req,res) =>{
     res.send({hi:'gotohell'});
