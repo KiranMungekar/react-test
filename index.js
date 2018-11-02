2c39e66f36d398791824e8737e5ab5407517d801
Kin_dada <mungekar.kiran30@gmail.com>
2018-11-02 16:36:49 +0530

    OAuth setup completed

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ const app = express();
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const PORT = process.env.PORT || 5000;
+const keys = require('./config/keys');
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret:keys.googleClientSecret,
+    callbackURL:'/auth/google/callback'
+},(accessToken,refreshToken,profile)=>{
+    console.log(accessToken);
+    console.log(refreshToken);
+    console.log(profile);
+}));
+
+app.get('/auth/google',
+    passport.authenticate('google',{
+     scope:['profile','email']
+}));
+
+app.get('/auth/google/callback',passport.authenticate('google'));
 
 app.get('/',(req,res) =>{
     res.send({hi:'gotohell'});
