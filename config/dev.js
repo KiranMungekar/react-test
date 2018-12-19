26ec2456a4500fc0208cd36fedd35ae8be761680
Kin_dada <mungekar.kiran30@gmail.com>
2018-12-19 22:00:31 +0530

    build failing

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    googleClientID :'642531455899-rm4bqj609nbo47eora49bvlhtthponpm.apps.googleusercontent.com',
+    googleClientSecret :'ey-DnTdo-_7kx54NK0ZCZSyx',
+    mongoDevURI:'mongodb://kiran:kiran007@ds241493.mlab.com:41493/emaily_test_database',
+    cookieKey:'dhalkhdawhabfbbxvmnxbvohvahfagfaof'
+};
+
