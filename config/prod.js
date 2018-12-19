ac0ea05bbcd98d4ade973a141d499cd411ca824e
Kin_dada <mungekar.kiran30@gmail.com>
2018-12-19 21:54:10 +0530

    Prod keys generated

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,5 @@ module.exports = {
     googleClientID :process.env.GOOGLE_CLIEND_ID,
     googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
     mongoDevURI: process.env.MONGO_URI,
-    cookieKey:  process.env.COOKIE.KEY
+    cookieKey:  process.env.COOKIE_KEY
 };
