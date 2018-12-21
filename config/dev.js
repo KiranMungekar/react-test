7d044e03a93af41e41008dc9f3e348698d432673
Kin_dada <mungekar.kiran30@gmail.com>
2018-12-21 22:03:23 +0530

    jugad chalu

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,8 @@
 module.exports = {
-    googleClientID :'642531455899-rm4bqj609nbo47eora49bvlhtthponpm.apps.googleusercontent.com',
-    googleClientSecret :'ey-DnTdo-_7kx54NK0ZCZSyx',
-    mongoDevURI:'mongodb://kiran:kiran007@ds241493.mlab.com:41493/emaily_test_database',
-    cookieKey:'dhalkhdawhabfbbxvmnxbvohvahfagfaof'
+    googleClientID :'45971042153-hl8fp8bspa8nl9kkphl5s824oa4oiqat.apps.googleusercontent.com',
+    googleClientSecret :'ny8FZCLnD8aAtsHMAdjHzaw3',
+    mongoDevURI:'mongodb://Admin:Admin007@ds053972.mlab.com:53972/emaily_prod_db',
+    cookieKey:'dhawygddugawiuhduhawkdbawhdia'
 };
 
 
