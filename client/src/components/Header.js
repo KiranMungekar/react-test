4f8a47aeb2de8916bd9f9e4e2736b3911c3fcbc4
Kin_dada <mungekar.kiran30@gmail.com>
2019-01-10 23:34:58 +0530

    /src/index  const store = createStore(reducers, {}, applyMiddleware(reduxThunk));

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,26 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Component} from 'react';
+
 import { connect } from 'react-redux';
- 
+
 class Header extends Component {
-  render() {
-    console.log(this.props);
-    return (
-      <nav>
-        <div className="nav-wrapper">
-          <a className="left brand-logo">
-            Emaily
-          </a>
-          <ul className="right">
-            <li>
-              <a>Login with Google</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        console.log(this.props);
+        return ( 
+            <nav>
+                <div className="nav-wrapper container">
+                <a href="#" className="brand-logo">Emaily</a>
+                     <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="#">Login</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                </div>
+            </nav>
+         );
+    }
+ 
 }
-function mapStateToProps({ auth }) {
-  return { auth };
+function mapStateToProps({auth}){
+    console.log("auth "+ auth);
+    return{ auth:auth };
 }
-export default connect(mapStateToProps)(Header);
+export default connect (mapStateToProps)(Header);
