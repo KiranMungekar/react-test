d78a0f6613a255b71f0dabb04d94c471f12ce479
Kin_dada <mungekar.kiran30@gmail.com>
2019-01-10 23:01:46 +0530

    error solved by coping code

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,15 @@
+import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
-import  '../node_modules/materialize-css/dist/css/materialize.min.css'
-
 import reduxThunk from 'redux-thunk';
-
-//component
+ 
 import App from './components/App';
-
-// Reducers
 import reducers from './reducers';
-
-//Action Creaters
-
-const store = createStore(()=>reducers, {}, applyMiddleware(reduxThunk));
-
+ 
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+ 
 ReactDOM.render(
-    <Provider store={store}><App/></Provider>,
-     document.querySelector('#root')
-);
+  <Provider store={store}><App /></Provider>,
+  document.querySelector('#root'));
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,11 +1,12 @@
-import {FETCH_USER} from '../actions/types'
-
-export default function(state= null, action){
-    console.log(action);
-    switch(action.type){
-        case FETCH_USER:
-            return action.payload || false;
-        default:
-            return state;
-    }
-}
+import  {FETCH_USER} from '../actions/types';
+//null - we dont know if the user loged in.
+// user model - the user loged in.
+// false - the user not loged in.
+export default function(state = null, action) {
+    switch (action.type) {
+      case FETCH_USER:
+        return action.payload || false;
+      default:
+        return state;
+     }
+   }
