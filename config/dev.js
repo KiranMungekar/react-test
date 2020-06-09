00328f9d55033cffd2a69254d7cdd7bb84c92331
Kiran Mungekar <mungekar.kiran30@gmail.com>
2020-06-10 00:40:13 +0530

    a a tags are working now

--- config/dev.js
+++ config/dev.js
@@ -6,7 +6,7 @@ module.exports = {
     stripePublicKey:'pk_test_ozqvZ8Vhblu78sExM1Sesinq',
     stripePrivateKey:'sk_test_UV9uxZIdUp3vCIg94onwnizG',
     sendGridKey:'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',
-    redirectDomain:'localhost:3000'
+    redirectDomain:'https://localhost:3000'
 };
 
 
--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -4,14 +4,14 @@ module.exports = survey =>{
     return `
         <html>
             <body>
-              <div style="text-align:center">
+              <div style=\"text-align:center\">
               <h3>We would love your input</h3> 
-                <p>Please answer the following quetion:</p>
+                <p>Please answer the following question:</p>
                 <p>${survey.body}</p>
                 <br/>
-                <a href="localhost:3000">Test Click</a>
-                <p><a href="${keys.redirectDomain}/api/surveys/thanks">YES</a></p>
-                <p><a href="${keys.redirectDomain}/api/surveys/thanks">NO</a></p>
+                <a href=\"${keys.redirectDomain}\">Select the option</a>
+                <p><a href=\"${keys.redirectDomain}/api/surveys/thanks\">YES</a></p>
+                <p><a href=\"${keys.redirectDomain}/api/surveys/thanks\">NO</a></p>
               </div>
             </body>
         </html>
